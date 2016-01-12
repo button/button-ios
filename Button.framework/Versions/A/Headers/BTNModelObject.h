259846a201587f89b83ef33e247165ef9dd0d582
Wes Smith <wes@w5mith.com>
2016-01-12 15:11:55 -0500

    Release 4.2.0

--- Button.framework/Versions/A/Headers/BTNModelObject.h
+++ Button.framework/Versions/A/Headers/BTNModelObject.h
@@ -10,4 +10,24 @@
  */
 + (BOOL)canInitWithDictionary:(NSDictionary *)dictionary NS_REQUIRES_SUPER;
 
+
+
+///---------------------------------------
+/// @name Serializing/Deserializing Arrays
+///---------------------------------------
+
+/**
+ Returns a new array of objects from an array of dictionaries.
+ @param representations An array of dictionary representations to be converted into objects.
+ @note Non NSDictionary objects will be omitted.
+ */
++ (NSArray *)arrayOfObjectsFromRepresentations:(NSArray <NSDictionary *> *)representations;
+
+
+/**
+ Returns a serialized array of dictionaries from an array of BTNModelObject objects.
+ @note Objects not conforming to BTNSerializable will be omitted.
+ */
++ (NSArray <NSDictionary *> *)arrayOfRepresentationsFromObjects:(NSArray <BTNModelObject *> *)objects;
+
 @end
--- Button.podspec
+++ Button.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
 
   s.name    = "Button"
-  s.version = "4.1.0"
+  s.version = "4.2.0"
   s.summary = "Button iOS SDK."
 
   s.description = <<-DESC
